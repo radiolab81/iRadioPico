6768d865ea3bb6be78dc4c91408d8d1664f432b7
BM45 <48355256+BM45@users.noreply.github.com>
2024-04-23 06:47:29 +0200

    Update globals.cpp

--- src/modules/globals.cpp
+++ src/modules/globals.cpp
@@ -1,7 +1,7 @@
 #include "globals.h"
 
 // set by wifi.txt and playlist.m3u from SDCARD
-#define MAX_CHANNELS_IN_LIST 100
+
 char* playlist[MAX_CHANNELS_IN_LIST];
 unsigned int channels_in_list;
 unsigned int actual_channel_or_file_ID;
