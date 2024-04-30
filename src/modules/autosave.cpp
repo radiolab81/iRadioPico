a5860acca8972fdce9b354ddd89f848de0959a1f
BM45 <48355256+BM45@users.noreply.github.com>
2024-04-30 08:50:45 +0200

    Update autosave.cpp

--- src/modules/autosave.cpp
+++ src/modules/autosave.cpp
@@ -7,6 +7,8 @@
 
 static unsigned long last_check_time = 0;
 static unsigned int  last_actual_channel_or_file_ID = 0;
+static unsigned int  last_volume_L = 0;
+static unsigned int  last_volume_R = 0;
 
 void auto_save_init() {
 
@@ -16,12 +18,15 @@ void auto_save_run() {
 
    if (millis()-last_check_time > INTERVAL_LOOKING_FOR_CHANGE_IN_RADIO_SETTINGS) {
      // check if somethk changed since last run
-     if (actual_channel_or_file_ID != last_actual_channel_or_file_ID) {
+     if ((actual_channel_or_file_ID != last_actual_channel_or_file_ID) ||
+     	 (volume_L != last_volume_L ) ||
+     	 (volume_R != last_volume_R )) {
        writeRadioSettingsLittleFS();
-     }  
-       
+     }
    
      last_actual_channel_or_file_ID = actual_channel_or_file_ID;  
+     last_volume_L = volume_L;
+     last_volume_R = volume_R;
      last_check_time=millis();
    }
 }
