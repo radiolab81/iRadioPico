d1812bac324a97ba9908d72b7fd8e0f17e6044d3
BM45 <48355256+BM45@users.noreply.github.com>
2024-05-03 08:57:06 +0200

    Update gpiod_clockradio.cpp

--- src/modules/display/ClockRadio/gpiod_clockradio.cpp
+++ src/modules/display/ClockRadio/gpiod_clockradio.cpp
@@ -1,6 +1,7 @@
 #include "../../player.h"
 #include <Arduino.h>
 #include "../../globals.h"
+#include "../../lfs.h"
 
 #define KEY_CHANNEL_UP_PIN 28
 #define KEY_CHANNEL_DOWN_PIN 27
@@ -13,11 +14,9 @@ enum _result { NONE, CH_UP, CH_DOWN, ON_OFF };
 static unsigned long last_interrupt_time = 0;
 static _result result = NONE;
 
-static void isr_clockradio_keys() {
-  uint32_t flags = save_and_disable_interrupts();
-  //hw_clear_bits(&(iobank0_hw->proc0_irq_ctrl.inte[KEY_.... / 8]), 0b1111 << (KEY_.... % 8 )*4u );
+static void handle_clockradio_keys() {
    
-  //SERIAL.println("in isr");
+  if ((digitalRead(KEY_CHANNEL_UP_PIN)==0) ||  (digitalRead(KEY_CHANNEL_DOWN_PIN)==0) ||  (digitalRead(KEY_ON_OFF_PIN)==0))
   if (millis()-last_interrupt_time>DEBOUNCE_TIME_KEYS) {
   
     if (digitalRead(KEY_CHANNEL_UP_PIN)==0){
@@ -35,8 +34,6 @@ static void isr_clockradio_keys() {
     last_interrupt_time=millis();
   } // if (millis()-last_interrupt_time>DEBOUNCE_TIME_KEYS){
   
-  restore_interrupts(flags); 
-  //hw_set_bits(&(iobank0_hw->proc0_irq_ctrl.inte[KEY_.... / 8]), 0b0100 << (KEY_.... % 8 )*4u );
 }
 
 
@@ -45,12 +42,10 @@ void gpiod_clockradio_init() {
   pinMode(KEY_CHANNEL_UP_PIN,INPUT_PULLUP);
   pinMode(KEY_CHANNEL_DOWN_PIN,INPUT_PULLUP);
   pinMode(KEY_ON_OFF_PIN,INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(KEY_CHANNEL_UP_PIN), isr_clockradio_keys, FALLING);
-  attachInterrupt(digitalPinToInterrupt(KEY_CHANNEL_DOWN_PIN), isr_clockradio_keys, FALLING);
-  attachInterrupt(digitalPinToInterrupt(KEY_ON_OFF_PIN), isr_clockradio_keys, FALLING);
 }
 
 void gpiod_clockradio_run() {
+  handle_clockradio_keys();
   if (result == CH_UP)
     next_station();
     
@@ -66,7 +61,7 @@ void gpiod_clockradio_run() {
       radiomode = INTERNETRADIO;
       SERIAL_PORT.println("GPIOD_CLOCKRADIO: go to mode -> INTERNETRADIO");
     }
-    
+    writeRadioSettingsLittleFS();
   } // if (result == ON_OFF) {
   
   result = NONE;
