e50fa4ee3338fe1939f33b6ef0612965beb9b82b
BM45 <48355256+BM45@users.noreply.github.com>
2024-05-03 08:57:24 +0200

    Update gpiod_clockradio.cpp

--- src/modules/display/ClockRadio/gpiod_clockradio.cpp
+++ src/modules/display/ClockRadio/gpiod_clockradio.cpp
@@ -20,7 +20,7 @@ static void handle_clockradio_keys() {
   if (millis()-last_interrupt_time>DEBOUNCE_TIME_KEYS) {
   
     if (digitalRead(KEY_CHANNEL_UP_PIN)==0){
-	  result = CH_UP;
+      result = CH_UP;
     }
       
     if (digitalRead(KEY_CHANNEL_DOWN_PIN)==0){
